a0f4b24eb730fe78b1f8c32b0809b011a7beee18
IseEkko <794776085@qq.com>
2021-08-30 20:49:49 +0800

    init2

---
+++ app/Models/Completion2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Completion2 extends Model
+{
+    protected $table = "completion2";
+    public $timestamps = true;
+    protected $primaryKey = "id";
+    protected $guarded = [];
+}
---
+++ app/Models/Completion8.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Completion8 extends Model
+{
+    protected $table = "completion8";
+    public $timestamps = true;
+    protected $primaryKey = "id";
+    protected $guarded = [];
+
+}
