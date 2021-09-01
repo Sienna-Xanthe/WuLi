ccd12dbf906417e5bd08375f59ec10bd6666ad52
zqz <2435454360@qq.com>
2021-09-01 20:07:09 +0800

    wuli 11-pdf

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::prefix('experiment')->group(function (){
     /**
      * @Author: Alexcutest
-     */    
+     */
     Route::post('student','ExperimentController@student');//学生信息
-    
+
     Route::post('completion','ExperimentController@completion');//实验答题
 
 
     Route::get('pdf','ExperimentController@pdf');//实验pdf
-    
+
 });
+Route::get('pdf','Completion11Controller@pdf');//实验11pdf
